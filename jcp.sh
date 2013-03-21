8d13e8388657edc4f16e836b866ac9419a01a0b8
Timm Friebe <thekid@thekid.de>
2013-03-21 16:38:19 +0100

    - If a CLASSPATH environment variable is set, add to that

--- jcp.sh
+++ jcp.sh
@@ -7,19 +7,21 @@ else
 fi
 
 _jcp() {
-  JCP=""
-  while read line || [ -n "$line" ] ; do
-    case $line in
-      '#'*)
-        ;;
-      '!'*)
-        JCP=$SEP${line#"!"}$JCP
-        ;;
-      *)
-        JCP=$JCP$SEP$line
-        ;;
-    esac
-  done < class.jcp
+  JCP=${CLASSPATH-"."}
+  if [ -f class.jcp ] ; then
+    while read line || [ -n "$line" ] ; do
+      case $line in
+        '#'*)
+          ;;
+        '!'*)
+          JCP=$SEP${line#"!"}$JCP
+          ;;
+        *)
+          JCP=$JCP$SEP$line
+          ;;
+      esac
+    done < class.jcp
+  fi
   echo ${JCP#$SEP}
 }
 
@@ -29,19 +31,13 @@ _jdk() {
   else 
     EXE=$JAVA_HOME/bin/$EXE
   fi
-  if [ -f class.jcp ] ; then
-    $EXE -cp "$(_jcp)" "$@"
-  else
-    $EXE "$@"
-  fi
+  $EXE -cp "$(_jcp)" "$@"
 }
 
 jcp() {
   echo "JCP 1.0 (c) 2013 https://github.com/thekid/jcp"
   EXE=java _jdk -version 2>&1 | head -1
-  if [ -f class.jcp ] ; then
-    _jcp
-  fi
+  _jcp
 }
 
 java() {
